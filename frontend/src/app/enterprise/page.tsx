11d803b4283a0a460401ceb57381b9dfce429746
Abdelilah Elgallati <abdelilah.elgallati6@gmail.com>
2026-02-08 20:46:14 +0100

    Add backend logic for data science and analyse and meetings part

--- frontend/src/app/enterprise/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { EnterpriseView } from '../../views/enterprise/EnterpriseView';
-
-export default function EnterprisePage() {
-    return <EnterpriseView />;
-}
--- frontend/src/app/visitor/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { VisitorView } from '../../views/visitor/VisitorView';
-
-export default function VisitorPage() {
-    return <VisitorView />;
-}
